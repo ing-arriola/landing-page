578a6c29977f09badace7d408fa54c23489c1ed6
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-31 02:13:39 -0600

    fixed issues with API, now getting data without problems also now data is filtered and I just need the pass filtered data to a Leaflet's component to show locations on map

--- src/Components/Restaurants/Restaurants.js
+++ src/Components/Restaurants/Restaurants.js
@@ -1,45 +1,74 @@
 import React, { useState, useEffect } from "react";
+
 import MapView from "./MapView";
 import Row from "react-bootstrap/Row";
 import { Tab, Tabs, Form, Col } from "react-bootstrap";
 
 const Restaurants = () => {
-  const [items, setItems] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState({});
+  const [filterItems, setFilterItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [key, setKey] = useState("takeaway");
+
   useEffect(() => {
     fetch("https://api.elaniin.dev/api/locations?type=takeaway")
       .then((res) => res.json())
       .then((result) => {
-        setIsLoaded(true);
         setItems(result);
+        setIsLoading(false);
       });
   }, []);
+
+  const changerImputHandler = (e) => {
+    let locationToSearch;
+    if (e.target.value !== "") {
+      locationToSearch = e.target.value;
+      const nearestLocation = items.data.filter((location) =>
+        location.address.toLowerCase().includes(locationToSearch.toLowerCase())
+      );
+      console.log(filterItems);
+      setFilterItems(nearestLocation);
+      console.log(nearestLocation);
+    }
+  };
 
   return (
     <div className="restaurantes">
       <Row>
         <Col>
           <h2>Estamos para ti</h2>
-          <Tabs className="first" defaultActiveKey="profile">
-            <Tab eventKey="takeaway" title="Para llevar"></Tab>
-            <Tab eventKey="delivery" title="Domicilio"></Tab>
+          <Tabs
+            className="first"
+            defaultActiveKey="profile"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+          >
+            <Tab eventKey="takeaway" title="Para llevar">
+              Para llevar
+            </Tab>
+            <Tab eventKey="delivery" title="Domicilio">
+              Domicilio
+            </Tab>
           </Tabs>
 
           <Form.Group className="searchName">
-            <Form.Control size="sm" type="text" placeholder="Small text" />
+            <Form.Control
+              onChange={changerImputHandler}
+              size="sm"
+              type="text"
+              placeholder="Small text"
+            />
           </Form.Group>
-
-          {!isLoaded ? (
-            <div>Loading...</div>
-          ) : (
-            <ul>
-              {items.data.map((item) => (
-                <li key={item.name}>{item.name}</li>
-              ))}
-            </ul>
-          )}
         </Col>
-
+        {!filterItems ? (
+          <p>cargando....</p>
+        ) : (
+          <ul>
+            {filterItems.map((item) => (
+              <li key={item.name}>{item.name}</li>
+            ))}
+          </ul>
+        )}
         <Col>
           <MapView />
         </Col>
@@ -47,5 +76,4 @@ const Restaurants = () => {
     </div>
   );
 };
-
 export default Restaurants;
